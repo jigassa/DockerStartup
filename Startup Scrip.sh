d6b4204781ed2521f5298534a44895a6710f931e
jigassa <31981842+jigassa@users.noreply.github.com>
2022-02-12 13:39:19 -0600

    GH Add

--- Startup Scrip.sh
+++ Startup Scrip.sh
@@ -1,7 +1,7 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update && upgrade -y
 sudo apt install -y nmap wireshark tcpdump tcptrack aircrack-ng atop htop glances snap git wget vlc obs-studio notepadqq ca-certificates curl gnupg lsb-release screen
-sudo snap install bashtop
+sudo snap install bashtop gh
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
