c217e3cadace991f816e9cd8858075f06c4386f3
jigassa <31981842+jigassa@users.noreply.github.com>
2022-02-12 13:33:18 -0600

    Initial Scrip
    Upload of a initial Script to startup the Docker Server

---
+++ Startup Scrip.sh
@@ -0,0 +1,14 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update && upgrade -y
+sudo apt install -y nmap wireshark tcpdump tcptrack aircrack-ng atop htop glances snap git wget vlc obs-studio notepadqq ca-certificates curl gnupg lsb-release screen
+sudo snap install bashtop
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+groups
+newgrp docker
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
