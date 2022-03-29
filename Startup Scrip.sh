76b12f0249624f8d5d0f99bba5bfbb58c935e5eb
jigassa <31981842+jigassa@users.noreply.github.com>
2022-03-29 13:44:20 -0600

    Update Startup Scrip.sh

--- Startup Scrip.sh
+++ Startup Scrip.sh
@@ -1,6 +1,6 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update && upgrade -y
-sudo apt install -y nmap wireshark tcpdump tcptrack aircrack-ng atop htop glances snap git wget vlc obs-studio notepadqq ca-certificates curl gnupg lsb-release screen
+sudo apt install -y nmap tcpdump tcptrack aircrack-ng atop htop glances snap git wget ca-certificates curl gnupg lsb-release screen
 sudo snap install bashtop gh
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -11,4 +11,4 @@ sudo usermod -aG docker $USER
 groups
 newgrp docker
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
