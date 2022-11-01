a8cdae716456f17a876dc3172cf2a7175329d51c
jigassa <31981842+jigassa@users.noreply.github.com>
2022-11-01 12:51:03 -0600

    Update Startup Scrip.sh

--- Startup Scrip.sh
+++ Startup Scrip.sh
@@ -1,6 +1,6 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update && sudo apt upgrade -y
-sudo apt install -y nmap tcpdump tcptrack aircrack-ng atop htop glances git wget ca-certificates btop iftop powertop curl gnupg lsb-release screen
+sudo apt install -y nmap tcpdump tcptrack aircrack-ng atop htop glances git wget ca-certificates btop iftop powertop curl gnupg lsb-release screen nfs-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
