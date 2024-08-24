272cbbcb73d2e8289d625ebe49001f9537407182
jigassa <31981842+jigassa@users.noreply.github.com>
2024-08-24 17:42:46 -0600

    Update start.sh
    Forma simple para instalar docker en 22.04 y 24.04 ubuntu

--- Startup Scrip.sh
+++ Startup Scrip.sh
@@ -1,13 +1,14 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update && sudo apt upgrade -y
-sudo apt install -y nmap tcpdump tcptrack aircrack-ng atop htop glances git wget ca-certificates btop iftop powertop curl gnupg lsb-release screen nfs-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo groupadd docker
-sudo usermod -aG docker $USER
-groups
-newgrp docker
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo apt-get upgrade -y
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo docker run hello-world
