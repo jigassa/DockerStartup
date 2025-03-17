6bdbc2a9b320280c82c3b86e58f03dde413c7f3f
jigassa <31981842+jigassa@users.noreply.github.com>
2025-03-16 23:30:02 -0600

    Update Startup Scrip.sh
    Le regreso el groups, porque las nuevas versiones estan mas jodidas

--- Startup Scrip.sh
+++ Startup Scrip.sh
@@ -11,4 +11,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+groups
+newgrp docker
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 sudo docker run hello-world
